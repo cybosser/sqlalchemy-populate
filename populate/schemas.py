ae2b1168618f1c926e0d8242295f64d61322d422
Sergey Zubov <zubov@spb.gs.ru>
2016-04-02 17:48:30 +0300

    add fixtures' schema

---
+++ populate/schemas.py
@@ -0,0 +1,13 @@
+FIXTURES_SCHEMA = {
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'properties': {
+            'model': { 'type': 'string' },
+            # TODO: add non-integer primary keys support
+            'pk': { 'type': 'integer' },
+            'fields': { 'type': 'object' }
+        },
+        'required': [ 'model' ]
+    }
+}
