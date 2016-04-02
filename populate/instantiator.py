afe64b312c8228999caa20fcaa97562ce0a37fe9
Sergey Zubov <zubov@spb.gs.ru>
2016-04-02 17:31:42 +0300

    extract instantiate function into a separate module

---
+++ populate/instantiator.py
@@ -0,0 +1,17 @@
+import importlib
+
+from populate.parsers import parse_model_name
+
+
+def _get_model_class(model_name):
+    module_name, class_name = parse_model_name(model_name)
+
+    module = importlib.import_module(module_name)
+
+    return getattr(module, class_name)
+
+
+def instantiate_model(model_name, pk, fields):
+    model_class = _get_model_class(model_name)
+
+    return model_class(**fields)
--- populate/populator.py
+++ populate/populator.py
@@ -1,28 +1,14 @@
-import importlib
 import itertools
 
-from populate.parsers import parse_model_name
-
-
-def _get_model_class(model_name):
-    module_name, class_name = parse_model_name(model_name)
-
-    module = importlib.import_module(module_name)
-
-    return getattr(module, class_name)
-
-
-def _instantiate_impl(model_name, pk, fields):
-    model_class = _get_model_class(model_name)
-
-    model = model_class(**fields)
-    model.id = pk
-
-    return model
+from populate.instantiator import instantiate_model
 
 
 def _instantiate(fixture):
-    return _instantiate_impl(fixture['model'], fixture['pk'], fixture['fields'])
+    model = instantiate_model(fixture['model'], fixture['fields'])
+
+    model.id = fixture['pk']
+
+    return model
 
 
 def populate(session, fixtures, commit=True):
