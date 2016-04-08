d008c7b9d7a66ef2365e900ff19fa3d7213db108
Sergey Zubov <zubov@spb.gs.ru>
2016-04-08 21:01:00 +0300

    throw FormatError exception if model name is invalid

--- sqlalchemy_populate/parsers.py
+++ sqlalchemy_populate/parsers.py
@@ -1,9 +1,11 @@
 import re
 
+from sqlalchemy_populate.exceptions import FormatError
+
 
 def parse_model_name(model_name):
     match = re.search('^((?:\w+\.?)+)\.(\w+)$', model_name)
     if not match:
-        raise RuntimeError(u'Invalid model name: ' + model_name)
+        raise FormatError(u'Invalid model name: ' + model_name)
 
     return match.groups()
