fe3f086296d270358a790997575293507a4c8bf2
Sergey Zubov <zubov@spb.gs.ru>
2016-05-11 14:40:19 +0300

    parameterize tests

--- sqlalchemy_populate_tests/parsers_tests.py
+++ sqlalchemy_populate_tests/parsers_tests.py
@@ -1,27 +1,28 @@
 import unittest
 
+from nose_parameterized import parameterized
+
 
 class ParseModelNameTests(unittest.TestCase):
-    def test_valid_model_name_returns_module_and_class_names(self):
+    @parameterized.expand([
+        ('module.class', 'module', 'class'),
+        ('package.module.class', 'package.module', 'class'),
+    ])
+    def test_valid_model_name_returns_module_and_class_names(self, model_name, expected_module_name, expected_class_name):
         from sqlalchemy_populate.parsers import parse_model_name
 
-        MODEL_NAMES = (
-            ('module.model', 'module', 'model'),
-            ('package.module.model', 'package.module', 'model')
-        )
+        module_name, class_name = parse_model_name(model_name)
 
-        for model_name, module_name_expected, class_name_expected in MODEL_NAMES:
-            module_name, class_name = parse_model_name(model_name)
+        self.assertEqual(expected_module_name, module_name)
+        self.assertEqual(expected_class_name, class_name)
 
-            self.assertEqual(module_name, module_name_expected)
-            self.assertEqual(class_name, class_name_expected)
-
-    def test_invalid_model_name_throws_format_error(self):
+    @parameterized.expand([
+        'class',
+        'module/package',
+    ])
+    def test_invalid_model_name_throws_format_error(self, model_name):
         from sqlalchemy_populate.exceptions import FormatError
         from sqlalchemy_populate.parsers import parse_model_name
 
-        MODEL_NAMES = ('model', 'module/package')
-
-        for model_name in MODEL_NAMES:
-            with self.assertRaises(FormatError):
-                parse_model_name(model_name)
+        with self.assertRaises(FormatError):
+            parse_model_name(model_name)
