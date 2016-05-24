9fd2612defcbb04952b438b100f78347a80d7e98
Sergey Zubov <zubov@spb.gs.ru>
2016-05-24 12:24:35 +0300

    implement substitute_exception context manager

--- sqlalchemy_populate/exceptions.py
+++ sqlalchemy_populate/exceptions.py
@@ -1,6 +1,17 @@
+import contextlib
+
+
 class ValidationError(Exception):
     pass
 
 
 class FormatError(Exception):
     pass
+
+
+@contextlib.contextmanager
+def substitute_exception(expected_exception_class, exception_object):
+    try:
+        yield
+    except expected_exception_class:
+        raise exception_object
