7247f4e93cffa9714f0162c652db5812addd2c20
Sergey Zubov <zubov@spb.gs.ru>
2016-05-31 15:44:59 +0300

    implement test for validate_fixture function

---
+++ sqlalchemy_populate_tests/validator_tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+from nose_parameterized import parameterized
+
+
+class ValidateFixturesTest(unittest.TestCase):
+    def test__missing_model_name__throws_exception(self):
+        from sqlalchemy_populate.validator import validate_fixture
+        from sqlalchemy_populate.validator import ValidationError
+
+        fixture = {
+            'pk': 1,
+            'fields': {
+                'name': 'value'
+            }
+        }
+
+        with self.assertRaises(ValidationError):
+            validate_fixture(fixture)
+
+    @parameterized.expand([
+        ({'model': 'package.module.class'},),
+        ({'model': 'package.module.class', 'pk': 1},),
+        ({'model': 'package.module.class', 'pk': 1, 'fields': {}},),
+        ({'model': 'package.module.class', 'pk': 1, 'fields': {'name': 'value'}},)
+    ])
+    def test__valid_fixture__does_nothing(self, fixture):
+        from sqlalchemy_populate.validator import validate_fixture
+
+        validate_fixture(fixture)
