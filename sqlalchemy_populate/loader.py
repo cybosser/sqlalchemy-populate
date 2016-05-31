344968cde03b0a9ff7deae734f09fc011b5fd6fd
Sergey Zubov <zubov@spb.gs.ru>
2016-05-31 13:38:43 +0300

    fix pep8 violation

--- sqlalchemy_populate/loader.py
+++ sqlalchemy_populate/loader.py
@@ -2,6 +2,7 @@ import importlib
 
 from sqlalchemy_populate.exceptions import LoadError, substitute_exception
 
+
 def load_class(module_name, class_name):
     with substitute_exception(ImportError, LoadError("Couldn't import module '" + module_name + "'")):
         module = importlib.import_module(module_name)
